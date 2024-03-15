6d8a9358619359660c7392ec08c7c77293a856d7
Greg Pilkington <greg.j.pilkington@gmail.com>
2024-03-15 12:00:35 -0600

    bonus

--- screens/AboutScreen.jsx
+++ screens/AboutScreen.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text} from 'react-native';
 
 const AboutScreen = ({}) => {
+  const [message, setMessage] = React.useState('');
+
+  const apiCall = async () => {
+    const response = await fetch(
+      'https://corporatebs-generator.sameerkumar.website/',
+      {
+        method: 'GET',
+      },
+    );
+    const json = await response.json();
+
+    const {phrase} = json;
+    setMessage(phrase);
+  };
+  React.useEffect(() => {
+    apiCall().then(() => {});
+  }, []);
   return (
     <>
-      <Text style={styles.title}>Greg's Incredible List</Text>
+      <Text style={{fontSize: 18}}>Greg's Incredible List</Text>
       <Text style={{fontSize: 16}}>Written By: Greg Pilkington</Text>
       <Text style={{fontSize: 16}}>Date: March 7, 2024</Text>
+      <Text style={{fontSize: 16}}> {message}</Text>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    margin: 20,
-  },
-  body: {
-    fontSize: 16,
-    margin: 20,
-  },
-});
 
 export default AboutScreen;
